7ee48b3f599cd56d3d9aa0fb4b1d2c37b7d39d24
thiya-1st <taingarasothy@gmail.com>
2026-08-25 03:27:29 +0100

    structured roic calculation

--- src/config.py
+++ src/config.py
@@ -4,13 +4,18 @@ import pandas as pd
 START_DATE = "2022-01-01"
 END_DATE = "2025-12-31"
 
+FUNDAMENTALS_LAG_DAYS = 90  
+FUNDAMENTALS_MAX_STALENESS_MONTHS = 15
 
 PRICE_SEARCH_THRESHOLD_DAYS = 5  # max trading days to search backward for a valid price
-REBALANCE_FREQ = "QS" # quarterly
 MOMENTUM_MIN_HISTORY_MONTHS = 12  # longest lookback needed (12-1 month momentum)
 
+REBALANCE_FREQ = "QS" # quarterly
 REBALANCE_START_DATE = pd.Timestamp(START_DATE) + pd.DateOffset(months = MOMENTUM_MIN_HISTORY_MONTHS)
 REBALANCE_DATES = pd.date_range(start=REBALANCE_START_DATE, end=END_DATE, freq=REBALANCE_FREQ)
+
+FLAT_US_TAX_RATE_FALLBACK = 0.21
+FLAT_UK_TAX_RATE_FALLBACK = 0.25
 
 BALANCE_SHEET_REQUIRED_FIELDS = [
     "Cash And Cash Equivalents",
--- src/factors.py
+++ src/factors.py
@@ -7,7 +7,7 @@ def calculate_momentum(prices, snapshot_date, lookback_months, skip_num_months):
     pd_date = pd.Timestamp(snapshot_date)
 
     current_price = None
-    for i in range(config.PRICE_SEARCH_THRESHOLD_DAYS):
+    for i in range(config.PRICE_SEARCH_THRESHOLD_DAYS): #TODO: make into function if possible
         current_date = pd_date - pd.DateOffset(months = skip_num_months) - pd.Timedelta(days = i)
         try:
             current_price = prices.loc[current_date, "Adj Close"]
@@ -16,7 +16,7 @@ def calculate_momentum(prices, snapshot_date, lookback_months, skip_num_months):
             pass
 
     past_price = None
-    for i in range(config.PRICE_SEARCH_THRESHOLD_DAYS):
+    for i in range(config.PRICE_SEARCH_THRESHOLD_DAYS): #TODO: function
         past_date = pd_date - pd.DateOffset(months = lookback_months) - pd.Timedelta(days = i)
         try:
             past_price = prices.loc[past_date, "Adj Close"]
@@ -31,14 +31,14 @@ def calculate_momentum(prices, snapshot_date, lookback_months, skip_num_months):
 
 def build_momentum_table(universe):
     momentum_table = []
-    missing_prices_log = []
+    missing_files_log = []
     nan_log = []
 
     for ticker in universe["Ticker"]:
         try:
-            prices = pd.read_csv(f"../data/raw/prices/{ticker}.csv", index_col = "Date") ##TODO: add ../
+            prices = pd.read_csv(f"../data/raw/prices/{ticker}.csv", index_col = "Date") 
         except:
-            missing_prices_log.append({"ticker": ticker, "missing_file": "prices"})
+            missing_files_log.append({"ticker": ticker, "missing_file": "prices"})
             continue
 
         prices.index = pd.to_datetime(prices.index.astype(str).str[:10])
@@ -63,5 +63,61 @@ def build_momentum_table(universe):
                 "momentum_6": momentum_6,
                 "momentum_3": momentum_3
             })
+    #TODO: check if we need logs
+    return pd.DataFrame(momentum_table), pd.DataFrame(missing_files_log), pd.DataFrame(nan_log)
 
-    return pd.DataFrame(momentum_table), pd.DataFrame(missing_prices_log), pd.DataFrame(nan_log)
+def get_latest_fundamental_period(fundamentals, date):
+    pass
+
+def calculate_roic(country, balance_sheet, income_statement, snapshot_date):
+    pd_date = pd.Timestamp(snapshot_date)
+    latest_balance_sheet_period = get_latest_fundamental_period(balance_sheet, pd_date)
+    latest_income_statement_period = get_latest_fundamental_period(income_statement, pd_date)
+
+    if latest_income_statement_period is None or latest_balance_sheet_period is None:
+        return np.nan
+
+    ebit = income_statement.loc["EBIT", latest_income_statement_period]
+    total_debt = balance_sheet.loc["Total Debt", latest_balance_sheet_period]
+    equity = balance_sheet.loc["Total Equity Gross Minority Interest", latest_balance_sheet_period]
+    cash = balance_sheet.loc["Cash And Cash Equivalents", latest_balance_sheet_period]
+
+    if ebit is None or total_debt is None or equity is None or cash is None:
+        return np.nan
+
+    tax_provision = income_statement.loc["Tax Provision", latest_income_statement_period]
+    pretax_income = income_statement.loc["Pretax Income", latest_income_statement_period]
+
+    if tax_provision is not None and pretax_income is not None:
+        tax_rate = tax_provision / pretax_income
+    elif country == "US":
+        tax_rate = config.FLAT_US_TAX_RATE_FALLBACK
+    else:
+        tax_rate = config.FLAT_UK_TAX_RATE_FALLBACK
+
+    nopat = ebit * (1 - tax_rate)
+    invested_capital = total_debt + equity - cash
+
+    return nopat / invested_capital
+
+def build_quality(universe):
+    quality_table = []
+    missing_files_log = []
+
+    for ticker in universe["Ticker"]:
+        try:
+            balance_sheet = pd.read_csv(f"../data/raw/fundamentals/{ticker}/balance_sheet.csv", index_col = 0) #TODO: check
+        except:
+            missing_files_log.append({"ticker": ticker, "missing_file": "balance sheet"})
+            continue
+
+        try:
+            income_statement = pd.read_csv(f"../data/raw/fundamentals/{ticker}/income_statement.csv", index_col = 0) #TODO: check
+        except:
+            missing_files_log.append({"ticker": ticker, "missing_file": "income statement"})
+            continue
+
+        country = universe.loc[ticker, "Country"]
+
+        for date in config.REBALANCE_DATES:
+            roic = calculate_roic(country, balance_sheet, income_statement, date)
