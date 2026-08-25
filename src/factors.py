ced39df8c73993f3a681250e33864a0ad568ba22
thiya-1st <taingarasothy@gmail.com>
2026-08-25 16:10:19 +0100

    create functions to calculate gross margin and operating margin

--- src/factors.py
+++ src/factors.py
@@ -69,26 +69,22 @@ def build_momentum_table(universe):
 def get_latest_fundamental_period(fundamentals, date):
     pass
 
-def calculate_roic(country, balance_sheet, income_statement, snapshot_date):
-    pd_date = pd.Timestamp(snapshot_date)
-    latest_balance_sheet_period = get_latest_fundamental_period(balance_sheet, pd_date)
-    latest_income_statement_period = get_latest_fundamental_period(income_statement, pd_date)
-
+def calculate_roic(country, balance_sheet, income_statement, latest_balance_sheet_period, latest_income_statement_period):
     if latest_income_statement_period is None or latest_balance_sheet_period is None:
         return np.nan
-
+    #TODO: log if periods are different and check if fine
     ebit = income_statement.loc["EBIT", latest_income_statement_period]
     total_debt = balance_sheet.loc["Total Debt", latest_balance_sheet_period]
     equity = balance_sheet.loc["Total Equity Gross Minority Interest", latest_balance_sheet_period]
     cash = balance_sheet.loc["Cash And Cash Equivalents", latest_balance_sheet_period]
 
-    if ebit is None or total_debt is None or equity is None or cash is None:
+    if pd.isna(ebit) or pd.isna(total_debt) or pd.isna(equity) or pd.isna(cash):
         return np.nan
 
     tax_provision = income_statement.loc["Tax Provision", latest_income_statement_period]
     pretax_income = income_statement.loc["Pretax Income", latest_income_statement_period]
 
-    if tax_provision is not None and pretax_income is not None:
+    if not pd.isna(tax_provision) and not pd.isna(pretax_income):
         tax_rate = tax_provision / pretax_income
     elif country == "US":
         tax_rate = config.FLAT_US_TAX_RATE_FALLBACK
@@ -98,7 +94,31 @@ def calculate_roic(country, balance_sheet, income_statement, snapshot_date):
     nopat = ebit * (1 - tax_rate)
     invested_capital = total_debt + equity - cash
 
-    return nopat / invested_capital
+    return nopat / invested_capital #TODO: check if dividing by 0
+
+def calculate_gross_margin(income_statement, latest_income_statement_period):
+    if latest_income_statement_period is None:
+        return np.nan
+
+    gross_profit = income_statement.loc["Gross Profit", latest_income_statement_period]
+    total_revenue = income_statement.loc["Total Revenue", latest_income_statement_period]
+
+    if pd.isna(gross_profit) or pd.isna(total_revenue):
+        return np.nan
+
+    return gross_profit / total_revenue #TODO: check divide by 0
+
+def calculate_operating_margin(income_statement, latest_income_statement_period):
+    if latest_income_statement_period is None:
+        return np.nan
+
+    ebit = income_statement.loc["EBIT", latest_income_statement_period]
+    total_revenue = income_statement.loc["Total Revenue", latest_income_statement_period]
+
+    if pd.isna(ebit) or pd.isna(total_revenue):
+        return np.nan
+
+    return ebit / total_revenue #TODO: check divide by 0
 
 def build_quality(universe):
     quality_table = []
@@ -107,17 +127,31 @@ def build_quality(universe):
     for ticker in universe["Ticker"]:
         try:
             balance_sheet = pd.read_csv(f"../data/raw/fundamentals/{ticker}/balance_sheet.csv", index_col = 0) #TODO: check
-        except:
+        except FileNotFoundError:
             missing_files_log.append({"ticker": ticker, "missing_file": "balance sheet"})
             continue
 
         try:
             income_statement = pd.read_csv(f"../data/raw/fundamentals/{ticker}/income_statement.csv", index_col = 0) #TODO: check
-        except:
+        except FileNotFoundError:
             missing_files_log.append({"ticker": ticker, "missing_file": "income statement"})
             continue
 
         country = universe.loc[ticker, "Country"]
-
         for date in config.REBALANCE_DATES:
-            roic = calculate_roic(country, balance_sheet, income_statement, date)
+            latest_balance_sheet_period = get_latest_fundamental_period(balance_sheet, date)
+            latest_income_statement_period = get_latest_fundamental_period(income_statement, date)
+
+            roic = calculate_roic(country, balance_sheet, income_statement, latest_balance_sheet_period, latest_income_statement_period)
+            gross_margin = calculate_gross_margin(income_statement, latest_income_statement_period)
+            operating_margin = calculate_operating_margin(income_statement, latest_income_statement_period)
+
+            quality_table.append({
+                "ticker": ticker,
+                "date": date,
+                "roic": roic,
+                "gross margin": gross_margin,
+                "operating margin": operating_margin
+            })
+
+    return pd.DataFrame(quality_table)
