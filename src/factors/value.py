50f1ef151a917c4408baaff60b46dad4ec9c08ca
Adrian Balogun <balogunjabaar@gmail.com>
2026-09-01 01:53:41 +0100

    value component refactored

--- src/factors/value.py
+++ src/factors/value.py
@@ -1,101 +1,85 @@
-from src import config
-from src.factors.utils import get_latest_fundamental_period, has_missing_data
+from src.factors.utils import has_missing_data
 
-import pandas as pd
 import numpy as np
 
-def get_latest_price_date(prices, date):
-    pass
-
-def build_value_table(universe):
-    value_table = []
-    missing_files_log = []
-
-    latest_balance_sheet_period = get_latest_fundamental_period(balance_sheet, date)
-    latest_income_statement_period = get_latest_fundamental_period(income_statement, date)
-    latest_cash_flow_statement_period = get_latest_fundamental_period(cash_flow_statement, date)
-    latest_price_date = get_latest_price_date(prices, date)
-    #TODO: log if periods are different and check if fine
-
-    price_adj_close = prices.loc[latest_price_date, "Adj Close"]
-
-    earnings_per_share = income_statement.loc["EPS", latest_income_statement_period]
-    ebitda = income_statement.loc["EBITDA", latest_income_statement_period]
-    net_income = income_statement.loc["Net Income", latest_income_statement_period]
-
-    total_debt = balance_sheet.loc["Total Debt", latest_balance_sheet_period]
-    cash = balance_sheet.loc["Cash And Cash Equivalents", latest_balance_sheet_period]
+def calculate_ev_ebitda(total_debt, ebitda, cash, market_cap):
+    if has_missing_data(total_debt, ebitda, cash, market_cap) or ebitda == 0:
+        return np.nan
     
-    operating_cash_flow = cash_flow_statement.loc["Operating Cash Flow" , latest_cash_flow_statement_period]
-    capital_expenditure = cash_flow_statement.loc["Capital Expenditure" , latest_cash_flow_statement_period]
+    ev = market_cap + total_debt - cash
+    return ev / ebitda
 
-    ev_ebitda = calculate_ev_ebitda(
-        price_adj_close,
-        total_debt,
-        ebitda,
-        cash,
-        earnings_per_share,
-        net_income
-    )
+def calculate_fcf_yield(operating_cash_flow, capital_expenditure, market_cap):
+    if has_missing_data(operating_cash_flow, capital_expenditure, market_cap) or market_cap == 0:
+        return np.nan
+    
+    free_cash_flow = operating_cash_flow - capital_expenditure
+    return free_cash_flow / market_cap
+       
 
-    free_cash_flow_yield = calculate_fcf_yield(
-        price_adj_close,
-        operating_cash_flow,
-        capital_expenditure,
-        earnings_per_share,
-        net_income
-    )
+def calculate_earnings_yield(market_cap, net_income):
 
-    earnings_yield = calculate_earnings_yield(
-        price_adj_close,
-        net_income,
-        earnings_per_share
-    )
+    if has_missing_data(net_income, market_cap) or market_cap == 0:
+        return np.nan
+    
+    return net_income / market_cap
 
-    value_table.append({
+def build_value_table(
+        ticker, 
+        date, 
+        balance_sheet, 
+        cash_flow, 
+        income_statement, 
+        prices,
+        metadata,
+        latest_balance_sheet_period,
+        latest_income_statement_period,
+        latest_cash_flow_period,
+        latest_price_date,
+        values_table
+    ):
+
+    if latest_income_statement_period is not None:
+        ebitda = income_statement.loc["EBITDA", latest_income_statement_period]
+        net_income = income_statement.loc["Net Income", latest_income_statement_period]
+    else:
+        ebitda = net_income = np.nan
+
+    if latest_balance_sheet_period is not None:
+        total_debt = balance_sheet.loc["Total Debt", latest_balance_sheet_period]
+        cash = balance_sheet.loc["Cash And Cash Equivalents", latest_balance_sheet_period]
+    else:
+        total_debt = cash = np.nan
+
+    if latest_cash_flow_period is not None:
+        operating_cash_flow = cash_flow.loc["Operating Cash Flow" , latest_cash_flow_period]
+        capital_expenditure = cash_flow.loc["Capital Expenditure" , latest_cash_flow_period]
+    else:
+        operating_cash_flow = capital_expenditure = np.nan
+    
+    if latest_price_date is not None:
+        price_adj_close = prices.loc[latest_price_date, "Adj Close"] #TODO: same function as momentum
+    else:
+        price_adj_close = np.nan
+
+    try:
+        shares_outstanding = metadata.loc[ticker, "sharesOutstanding"]
+    except:
+        shares_outstanding = np.nan
+
+    if has_missing_data([price_adj_close, shares_outstanding]):
+        market_cap = np.nan
+    else:
+        market_cap = price_adj_close * shares_outstanding
+        
+    ev_ebitda = calculate_ev_ebitda(total_debt, ebitda, cash, market_cap)
+    free_cash_flow_yield = calculate_fcf_yield(operating_cash_flow, capital_expenditure, market_cap)
+    earnings_yield = calculate_earnings_yield(market_cap, net_income)
+
+    values_table.append({
         "ticker": ticker,
         "date": date,
         "ev_ebitda": ev_ebitda,
         "free cash flow yield": free_cash_flow_yield,
         "earnings yield": earnings_yield
-    })
-
-    return pd.DataFrame(value_table)
-
-
-def calculate_ev_ebitda(
-        price_adj_close, 
-        total_debt, 
-        ebitda, 
-        cash, 
-        earnings_per_share, 
-        net_income
-    ):
-    market_cap = price_adj_close * (net_income/earnings_per_share)
-    ev = market_cap + total_debt - cash
-    ev_ebitda = ev / ebitda
-    return ev_ebitda
-
-    # MAKE SURE MARKET CAP CALCULATION IS CORRECT (PRICES X SHARES OUTSTANDING)
-
-def calculate_fcf_yield(
-        price_adj_close, 
-        operating_cash_flow, 
-        capital_expenditure, 
-        earnings_per_share, 
-        net_income
-    ):
-    market_cap = price_adj_close * (net_income/earnings_per_share)
-    free_cash_flow = operating_cash_flow - capital_expenditure
-    fcf_yield = free_cash_flow / market_cap
-    return fcf_yield
-    
-
-def calculate_earnings_yield(
-        price_adj_close, 
-        net_income, 
-        earnings_per_share
-    ):
-    market_cap = price_adj_close * (net_income/earnings_per_share)
-    earnings_yield = net_income / market_cap
-    return earnings_yield
+    }) 
