cb57c8ef65b34d4f94306578c15a9a0356fd6bf5
Adrian Balogun <balogunjabaar@gmail.com>
2026-09-01 01:52:31 +0100

    value factor recoded

--- src/factors/value.py
+++ src/factors/value.py
@@ -11,80 +11,54 @@ def build_value_table(universe):
     value_table = []
     missing_files_log = []
 
-    for ticker in universe["Ticker"]:
-        try:
-            balance_sheet = pd.read_csv(f"../../data/raw/fundamentals/{ticker}/balance_sheet.csv", index_col = 0) #TODO: check
-        except FileNotFoundError:
-            missing_files_log.append({"ticker": ticker, "missing_file": "balance sheet"})
-            continue
+    latest_balance_sheet_period = get_latest_fundamental_period(balance_sheet, date)
+    latest_income_statement_period = get_latest_fundamental_period(income_statement, date)
+    latest_cash_flow_statement_period = get_latest_fundamental_period(cash_flow_statement, date)
+    latest_price_date = get_latest_price_date(prices, date)
+    #TODO: log if periods are different and check if fine
 
-        try:
-            income_statement = pd.read_csv(f"../../data/raw/fundamentals/{ticker}/income_statement.csv", index_col = 0) #TODO: check
-        except FileNotFoundError:
-            missing_files_log.append({"ticker": ticker, "missing_file": "income statement"})
-            continue
+    price_adj_close = prices.loc[latest_price_date, "Adj Close"]
 
-        try:
-            cash_flow_statement = pd.read_csv(f"../../data/raw/fundamentals/{ticker}/cash_flow_statement.csv", index_col = 0) #TODO: check
-        except FileNotFoundError:
-            missing_files_log.append({"ticker": ticker, "missing_file": "cash flow statement"})
-            continue
+    earnings_per_share = income_statement.loc["EPS", latest_income_statement_period]
+    ebitda = income_statement.loc["EBITDA", latest_income_statement_period]
+    net_income = income_statement.loc["Net Income", latest_income_statement_period]
 
-        try:
-            prices = pd.read_csv(f"../../data/raw/prices/{ticker}.csv", index_col = 0) #TODO: check
-        except FileNotFoundError:
-            missing_files_log.append({"ticker": ticker, "missing_file": "prices"}) #ROWS AND COLUMNS ARE SWAPPED
-            continue
+    total_debt = balance_sheet.loc["Total Debt", latest_balance_sheet_period]
+    cash = balance_sheet.loc["Cash And Cash Equivalents", latest_balance_sheet_period]
+    
+    operating_cash_flow = cash_flow_statement.loc["Operating Cash Flow" , latest_cash_flow_statement_period]
+    capital_expenditure = cash_flow_statement.loc["Capital Expenditure" , latest_cash_flow_statement_period]
 
-        for date in config.REBALANCE_DATES:
-            latest_balance_sheet_period = get_latest_fundamental_period(balance_sheet, date)
-            latest_income_statement_period = get_latest_fundamental_period(income_statement, date)
-            latest_cash_flow_statement_period = get_latest_fundamental_period(cash_flow_statement, date)
-            latest_price_date = get_latest_price_date(prices, date)
-            #TODO: log if periods are different and check if fine
+    ev_ebitda = calculate_ev_ebitda(
+        price_adj_close,
+        total_debt,
+        ebitda,
+        cash,
+        earnings_per_share,
+        net_income
+    )
 
-            price_adj_close = prices.loc[latest_price_date, "Adj Close"]
+    free_cash_flow_yield = calculate_fcf_yield(
+        price_adj_close,
+        operating_cash_flow,
+        capital_expenditure,
+        earnings_per_share,
+        net_income
+    )
 
-            earnings_per_share = income_statement.loc["EPS", latest_income_statement_period]
-            ebitda = income_statement.loc["EBITDA", latest_income_statement_period]
-            net_income = income_statement.loc["Net Income", latest_income_statement_period]
+    earnings_yield = calculate_earnings_yield(
+        price_adj_close,
+        net_income,
+        earnings_per_share
+    )
 
-            total_debt = balance_sheet.loc["Total Debt", latest_balance_sheet_period]
-            cash = balance_sheet.loc["Cash And Cash Equivalents", latest_balance_sheet_period]
-            
-            operating_cash_flow = cash_flow_statement.loc["Operating Cash Flow" , latest_cash_flow_statement_period]
-            capital_expenditure = cash_flow_statement.loc["Capital Expenditure" , latest_cash_flow_statement_period]
-
-            ev_ebitda = calculate_ev_ebitda(
-                price_adj_close,
-                total_debt,
-                ebitda,
-                cash,
-                earnings_per_share,
-                net_income
-            )
-
-            free_cash_flow_yield = calculate_fcf_yield(
-                price_adj_close,
-                operating_cash_flow,
-                capital_expenditure,
-                earnings_per_share,
-                net_income
-            )
-
-            earnings_yield = calculate_earnings_yield(
-                price_adj_close,
-                net_income,
-                earnings_per_share
-            )
-
-            value_table.append({
-                "ticker": ticker,
-                "date": date,
-                "ev_ebitda": ev_ebitda,
-                "free cash flow yield": free_cash_flow_yield,
-                "earnings yield": earnings_yield
-            })
+    value_table.append({
+        "ticker": ticker,
+        "date": date,
+        "ev_ebitda": ev_ebitda,
+        "free cash flow yield": free_cash_flow_yield,
+        "earnings yield": earnings_yield
+    })
 
     return pd.DataFrame(value_table)
 
